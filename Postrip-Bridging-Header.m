03234a9e5e02ae1e78f6e676ffd590bbdb1768f3
Pavol Polacek <ejko@seznam.cz>
2017-04-23 13:07:08 +0200

    Initial launch setting plus icons

--- Postrip-Bridging-Header.m
+++
@@ -1,9 +0,0 @@
-//
-//  Postrip-Bridging-Header.m
-//  Postrip
-//
-//  Created by Pavol Polacek on 05/01/2016.
-//  Copyright © 2017 Pavol Polacek. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
