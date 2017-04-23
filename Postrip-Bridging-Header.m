df3028980f71437f967532af26ee56f5a1d3844e
Pavol Polacek <ejko@seznam.cz>
2017-04-23 12:22:47 +0200

    Parse server initial configuration

---
+++ Postrip-Bridging-Header.m
@@ -0,0 +1,9 @@
+//
+//  Postrip-Bridging-Header.m
+//  Postrip
+//
+//  Created by Pavol Polacek on 05/01/2016.
+//  Copyright © 2017 Pavol Polacek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
